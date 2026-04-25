0d76c338fef7493a7656c9399b556e860cf5e83e
Ilya Dudelzak <DIA070611@gmail.com>
2026-04-25 18:56:12 +0300

    init

---
+++ sql/print_project_prices.sql
@@ -0,0 +1,8 @@
+SELECT 
+    'Project ' || p.ID AS NAME,
+    SUM(w.SALARY) * DATEDIFF('MONTH', p.START_DATE, p.FINISH_DATE) AS PRICE
+FROM project p
+JOIN project_worker pw ON p.ID = pw.PROJECT_ID
+JOIN worker w ON pw.WORKER_ID = w.ID
+GROUP BY p.ID, p.START_DATE, p.FINISH_DATE
+ORDER BY PRICE DESC;
